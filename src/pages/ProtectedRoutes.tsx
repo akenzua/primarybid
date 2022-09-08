e310125ad6951e62a6f9e58b15414e09c24e9dff
Akintade Ajibade <ajibade.akintade@gmail.com>
2022-09-09 00:20:44 +0100

    removed unused const

--- src/pages/ProtectedRoutes.tsx
+++ src/pages/ProtectedRoutes.tsx
@@ -2,7 +2,7 @@ import { Navigate, Outlet } from "react-router-dom";
 import { useAuth } from "../context/authContext";
 
 export const ProtectedRoutes = () => {
-  const { isAuthenticated, setIsAuthenticated } = useAuth();
+  const { isAuthenticated } = useAuth();
   console.log("isAuthenticated", isAuthenticated);
   return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
 };
