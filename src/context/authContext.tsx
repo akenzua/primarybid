4b6b3322b6eae9baed887fcebed712f5cec1f29b
Akintade Ajibade <ajibade.akintade@gmail.com>
2022-09-08 23:44:22 +0100

    created context

---
+++ src/context/authContext.tsx
@@ -0,0 +1,36 @@
+import {
+  createContext,
+  Dispatch,
+  ReactNode,
+  SetStateAction,
+  useContext,
+  useState,
+} from "react";
+
+type AuthContextProps = {
+  isAuthenticated: boolean;
+  setIsAuthenticated: Dispatch<SetStateAction<boolean>>;
+};
+const AuthContext = createContext({} as AuthContextProps);
+
+type AuthProviderProps = {
+  children: ReactNode;
+};
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }: AuthProviderProps) {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  return (
+    <AuthContext.Provider
+      value={{
+        isAuthenticated,
+        setIsAuthenticated,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+}
---
+++ src/context/cartContext.tsx
@@ -0,0 +1,92 @@
+import { useContext, createContext, ReactNode, useState } from "react";
+import { useLocalStorage } from "../hooks/useLocalStorage";
+
+type CartPtoviderProps = {
+  children: ReactNode;
+};
+
+type CartContextProps = {
+  getProductQuantity: (id: number) => number;
+  addToCart: (id: number) => void;
+  removeFromCart: (id: number) => void;
+  clearCart: () => void;
+  openCart: () => void;
+  closeCart: () => void;
+  cartQuantity: number;
+  cartItems: CartItem[];
+};
+
+type CartItem = {
+  id: number;
+  quantity: number;
+};
+
+const CartContext = createContext({} as CartContextProps);
+export function useCart() {
+  return useContext(CartContext);
+}
+
+export function CartProvider({ children }: CartPtoviderProps) {
+  const [cartItems, setCartItems] = useLocalStorage<CartItem[]>("cart", []);
+
+  const [cartOpen, setCartOpen] = useState(false);
+
+  const openCart = () => setCartOpen(true);
+  const closeCart = () => setCartOpen(false);
+
+  const cartQuantity = cartItems.reduce((acc, item) => acc + item.quantity, 0);
+
+  function getProductQuantity(id: number) {
+    const item = cartItems.find((item) => item.id === id);
+    return item ? item.quantity : 0;
+  }
+
+  function addToCart(id: number) {
+    const item = cartItems.find((item) => item.id === id);
+    if (item) {
+      setCartItems(
+        cartItems.map((item) =>
+          item.id === id ? { ...item, quantity: item.quantity + 1 } : item
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { id, quantity: 1 }]);
+    }
+  }
+
+  function removeFromCart(id: number) {
+    const item = cartItems.find((item) => item.id === id);
+    if (item) {
+      if (item.quantity === 1) {
+        setCartItems(cartItems.filter((item) => item.id !== id));
+      } else {
+        setCartItems(
+          cartItems.map((item) =>
+            item.id === id ? { ...item, quantity: item.quantity - 1 } : item
+          )
+        );
+      }
+    }
+  }
+
+  function clearCart() {
+    setCartItems([]);
+  }
+
+  return (
+    <CartContext.Provider
+      value={{
+        getProductQuantity,
+        addToCart,
+        removeFromCart,
+        clearCart,
+        cartItems,
+        openCart,
+        closeCart,
+        cartQuantity,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+}
